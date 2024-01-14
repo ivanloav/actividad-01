8755fca306e859ade8ab0818f9425a810844117d
Iván López Ávila <ivan@MacBook-Pro-de-Ivan.local>
2024-01-14 16:59:51 +0100

    Añadida clase A con atributos foo y bar

---
+++ clase-A.js
@@ -0,0 +1,14 @@
+class A {
+    constructor(foo, bar) {
+        this.foo = foo;
+        this.bar = bar;
+    }
+
+    getFoo() {
+        return this.foo;
+    }
+
+    getBar() {
+        return this.bar;
+    }
+}
